fb750233e48a5475bee5b9894d4800d597e11163
Artur Aronov <unionco@icloud.com>
2025-09-11 00:38:55 +0800

    wip

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/binary"
 	"fmt"
 	"log"
 	"net/http"
@@ -17,27 +19,44 @@ func compress(input string) ([]byte, error) {
 	return u[:], nil
 }
 
-func main() {
-	// body := []byte(`{"m": "h"}`)
-	// req, err := http.NewRequest("GET", "http://localhost:8080", bytes.NewReader(body))
-	tranactionType := 1                                     //1 byte
-	transactionMethod := 1                                  //1 byte
-	orderType := 1                                          //1 byte
-	ticker := "XYZQ"                                        //4 bytes
-	quantity := 4294967295                                  //?? bytes
-	price := 770                                            // ?? bytes
-	var orderDate uint64 = uint64(time.Now().Unix())        //?? bytes
-	var goodUntil uint64 = 18446744073709551615             //?? bytes
-	traderId := "46090691-dc41-4280-960f-3428806c3c5e"      //36 bytes
-	clientOrderId := "2527ee99-4f5c-4adf-8a10-dc5c7b7ee287" //36 bytes
+func uint64ToBytes(input uint64) []byte {
+	buf := make([]byte, 8)
+	binary.BigEndian.PutUint64(buf, input)
 
-	tickerByte := []byte(ticker)
-	quantityByte := byte(quantity)                  //?? bytes
-	priceByte := byte(price)                        //?? bytes
-	orderDateByte := byte(orderDate)                //?? bytes
-	goodUntilByte := byte(goodUntil)                //?? bytes
-	traderIdByte, _ := compress(traderId)           //?? bytes
-	clientOrderIdByte, _ := compress(clientOrderId) //?? bytes
+	return buf
+}
+
+func uint32ToBytes(input uint32) []byte {
+	buf := make([]byte, 4)
+	binary.BigEndian.PutUint32(buf, input)
+
+	return buf
+}
+
+func main() {
+	timeNow := time.Now()
+
+	tranactionType := 1                                            // 1 byte
+	transactionMethod := 1                                         // 1 byte
+	orderType := 1                                                 // 1 byte
+	ticker := "XYZQ"                                               // 4 bytes
+	quantity := 4294967295                                         // ?? bytes
+	price := 770                                                   // ?? bytes
+	var orderDate uint64 = uint64(timeNow.AddDate(1, 0, 0).Unix()) // ?? bytes
+	var goodUntil uint64 = 18446744073709551615                    // ?? bytes
+	traderId := "46090691-dc41-4280-960f-3428806c3c5e"             // 36 bytes
+	clientOrderId := "2527ee99-4f5c-4adf-8a10-dc5c7b7ee287"        // 36 bytes
+
+	tickerByte := []byte(ticker)                    // 4 bytes
+	quantityByte := make([]byte, 4)                 // 4 bytes
+	priceByte := make([]byte, 4)                    // 4 bytes
+	orderDateByte := uint64ToBytes(orderDate)       // 8 bytes
+	goodUntilByte := uint64ToBytes(goodUntil)       // 8 bytes
+	traderIdByte, _ := compress(traderId)           // 16 bytes
+	clientOrderIdByte, _ := compress(clientOrderId) // 16 bytes
+
+	binary.BigEndian.PutUint32(quantityByte, uint32(quantity))
+	binary.BigEndian.PutUint32(priceByte, uint32(price))
 
 	fmt.Printf("tranactionType: %v\n", tranactionType)
 	fmt.Printf("transactionMethod: %v\n", transactionMethod)
@@ -50,15 +69,31 @@ func main() {
 	fmt.Printf("traderIdByte: %v\n", traderIdByte)
 	fmt.Printf("clientOrderIdByte: %v %d\n", clientOrderIdByte, len(clientOrderIdByte))
 
-	req, err := http.NewRequest("GET", "http://localhost:8080", nil)
+	orderPayload := []byte{}
+
+	orderPayload = append(orderPayload, byte(tranactionType))
+	orderPayload = append(orderPayload, byte(transactionMethod))
+	orderPayload = append(orderPayload, byte(orderType))
+	orderPayload = append(orderPayload, tickerByte...)
+	orderPayload = append(orderPayload, quantityByte...)
+	orderPayload = append(orderPayload, priceByte...)
+	orderPayload = append(orderPayload, orderDateByte...)
+	orderPayload = append(orderPayload, goodUntilByte...)
+	orderPayload = append(orderPayload, traderIdByte...)
+	orderPayload = append(orderPayload, clientOrderIdByte...)
+	orderPayloadUrlEncoded := base64.URLEncoding.EncodeToString(orderPayload)
+	// decodedBytes, _ := base64.URLEncoding.DecodeString(orderPayloadUrlEncoded)
+
+	fmt.Println(orderPayload, len(orderPayload))
+	// fmt.Println(decodedBytes)
+	fmt.Println(orderPayloadUrlEncoded)
+
+	req, err := http.NewRequest("GET", "http://localhost:8080/"+orderPayloadUrlEncoded, nil)
 	if err != nil {
 		log.Fatalf("Failed to create request: %v", err)
 	}
 
-	// req.Header.Set("User-Agent", "_")
-	// req.Header.Set("Accept", "*/*")
-	// req.Header.Set("Content-Type", "application/json")
-	// Send the request
+	req.Header.Set("User-Agent", "market-broker")
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
